cab2cd65f39f4d20dcc4db34e59d8bb05ed9d646
Daniel Rotter <daniel.rotter@gmail.com>
2022-01-31 18:10:05 +0100

    Use jest-fetch-mock instead of implementing fetch mocks on our own

--- src/twitter/TwitterListRepository.test.ts
+++ src/twitter/TwitterListRepository.test.ts
@@ -1,44 +1,32 @@
+import {enableFetchMocks} from "jest-fetch-mock";
 import List from "../domain/List";
 import ListId from "../domain/ListId";
 import TwitterApiError from "./TwitterApiError";
 import TwitterListRepository from "./TwitterListRepository";
 
-function createFetchMockWithSuccessfulResponse(json: Object) {
-	const fetchMock = jest.fn();
+enableFetchMocks();
 
-	const jsonMock = jest.fn();
-	jsonMock.mockResolvedValue(json);
-
-	fetchMock.mockResolvedValue({json: jsonMock, ok: true});
-
-	return fetchMock;
-}
-
-function createFetchMockWithUnsuccessfulResponse() {
-	const fetchMock = jest.fn();
-	fetchMock.mockResolvedValue({json: jest.fn(), ok: false});
-
-	return fetchMock;
-}
+// https://github.com/jefflau/jest-fetch-mock/issues/184#issuecomment-718058715
+beforeEach(() => fetchMock.resetMocks());
 
 it("returns lists with id and name from response", async function() {
-	global.fetch = createFetchMockWithSuccessfulResponse([
+	fetchMock.mockResponse(JSON.stringify([
 		{id_str: "123", name: "topic-1"},
 		{id_str: "456", name: "topic-2"},
-	]);
+	]));
 
 	const twitterListRepository = new TwitterListRepository();
 	const lists = await twitterListRepository.findByScreenName("danrot90");
 
-	expect(global.fetch).toBeCalledWith("/api/lists/list.json?screen_name=danrot90");
+	expect(fetchMock).toBeCalledWith("/api/lists/list.json?screen_name=danrot90");
 	expect(lists).toEqual([new List("123" as ListId, "topic-1"), new List("456" as ListId, "topic-2")]);
 });
 
 it("returns empty list if response is unsuccessful", async function() {
-	global.fetch = createFetchMockWithUnsuccessfulResponse();
+	fetchMock.mockResponse(JSON.stringify({}), {status: 500});
 
 	const twitterListRepository = new TwitterListRepository();
 
 	expect(() => twitterListRepository.findByScreenName("danrot90")).rejects.toThrow(TwitterApiError);
-	expect(global.fetch).toBeCalledWith("/api/lists/list.json?screen_name=danrot90");
+	expect(fetchMock).toBeCalledWith("/api/lists/list.json?screen_name=danrot90");
 });
